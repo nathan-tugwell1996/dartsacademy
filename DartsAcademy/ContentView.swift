5f466329a93df2fe6737cf3c91f9c3621ae97d6e
Nathan Tugwell <nathan.tugwell@me.com>
2026-01-30 20:45:42 +0000

    Initial Commit

---
+++ DartsAcademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DartsAcademy
+//
+//  Created by Nathan Tugwell on 30/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DartsAcademy/DartsAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsAcademyApp.swift
+//  DartsAcademy
+//
+//  Created by Nathan Tugwell on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DartsAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
